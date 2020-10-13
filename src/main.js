33ea7ab5e799f43c4fc89deb9be183b88846c37e
CanCodes <32599057+CanCodes@users.noreply.github.com>
2020-10-13 18:32:32 +0300

    Basic version of update checking done.

--- src/main.js
+++ src/main.js
@@ -1,18 +1,43 @@
-const {BrowserWindow, app} = require("electron");
-const path = require("path")
+const {BrowserWindow, app, dialog} = require("electron");
+const path = require("path");
 const debug = require('electron-debug');
+const autoUpdater = require('update-electron-app');
 
-debug({showDevTools: false});
 
-function build() {
+debug({showDevTools: false, isEnabled: true});
+autoUpdater({repo: "CanCodes/Hiven-Desktop-Builds"});
+
+let loadingScreen;
+
+function createLoadingScreen() {
+    loadingScreen = new BrowserWindow({
+        width: 300,
+        height: 400,
+        frame: false,
+        transparent: true,
+        resizable: false
+    })
+    loadingScreen.loadFile(path.join(__dirname, '/views/loading.html'))
+    loadingScreen.setVisibleOnAllWorkspaces(true);
+    loadingScreen.setAlwaysOnTop(true, "floating", 1);
+    loadingScreen.on('closed', () => {
+        loadingScreen = null;
+    })
+    loadingScreen.webContents.on('did-finish-load', () => {
+        loadingScreen.show();
+    })
+}
+
+
+function createHivenClient() {
     const win = new BrowserWindow({
         width: 1280,
         height: 720,
         center: true,
         resizable: true,
+        show: false,
         webPreferences: {
-            devTools: true,
-            menubar:false
+            devTools: true
         }
     });
     win.webContents.session.setPreloads([path.join(__dirname, '/scripts/pgdmp.js')])
@@ -24,18 +49,22 @@ function build() {
     })
     win.loadURL("https://canary.hiven.io");
 
-    // win.setAutoHideMenuBar(true);
     win.setMenu(null)
     win.webContents.on('new-window', function(e, url) {
       e.preventDefault();
       require('electron').shell.openExternal(url);
     });
+    win.webContents.on('did-finish-load', () => {
+        loadingScreen.close();
+    })
+    win.show();
 }
 
 app.on("ready", () => {
-    build()
+    createLoadingScreen();
+    createHivenClient();
     app.on("activate", function() {
-        if(BrowserWindow.getAllWindows().length === 0) build();
+        if (BrowserWindow.getAllWindows().length === 0) createHivenClient();
     });
 
 });
@@ -47,3 +76,16 @@ app.on("window-all-closed", () => {
 });
 
 
+autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
+    const dialogOpts = {
+        type: 'info',
+        buttons: ['Install', 'Later'],
+        title: 'Application Update',
+        message: process.platform === 'win32' ? releaseNotes : releaseName,
+        detail: 'There is a new version for the Hiven Desktop App. Would you like to install the update?'
+    }
+
+    dialog.showMessageBox(dialogOpts).then((returnValue) => {
+        if (returnValue.response === 0) autoUpdater.quitAndInstall()
+    })
+})
