d784b28914126a6ab14a887c052a6fd6c828b9bc
CanCodes <32599057+CanCodes@users.noreply.github.com>
2020-11-03 20:35:14 +0300

    Removed auto update, added auto join when clicked on invite links.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,20 @@
-const {BrowserWindow, app, dialog} = require("electron");
+/* TODO: 
+    Check for updates.
+    Add "checking for updates" to the start up screen.
+    
+*/
+
+const { BrowserWindow, app, dialog } = require("electron");
 const path = require("path");
 const debug = require('electron-debug');
-const autoUpdater = require('update-electron-app');
+const request = require('request')
 
+// Disables errors dialogs on production. Comment this while debugging.
+dialog.showErrorBox = function (title, content) {
+    console.log(`${title}\n${content}`);
+};
 
-debug({showDevTools: false, isEnabled: true});
-autoUpdater({repo: "CanCodes/Hiven-Desktop-Builds"});
-
+debug({ showDevTools: false, isEnabled: true });
 let loadingScreen;
 
 function createLoadingScreen() {
@@ -50,42 +58,44 @@ function createHivenClient() {
     win.loadURL("https://canary.hiven.io");
 
     win.setMenu(null)
-    win.webContents.on('new-window', function(e, url) {
-      e.preventDefault();
-      require('electron').shell.openExternal(url);
-    });
     win.webContents.on('did-finish-load', () => {
         loadingScreen.close();
     })
     win.show();
+    win.webContents.on('new-window', async function (e, url) {
+        e.preventDefault();
+        if (url.includes('hiven.house/') || url.includes('hiven.io/invites/')) {
+            let key = await win.webContents.executeJavaScript("localStorage.getItem('hiven-auth')", true);
+            let link = `https://api.hiven.io/v1/invites/${url.split("/").pop()}`;
+            await request.get(link, (a, b, c) => {
+                let house_id = JSON.parse(c).data.house.id
+                request.post({
+                    url: link,
+                    headers: {
+                        "Authorization": key
+                    },
+                    method: 'POST'
+                }, (e, r, body) => {
+                    return win.loadURL(`https://canary.hiven.io/houses/${house_id}`)
+                })
+            })
+            return;
+        }
+        require('electron').shell.openExternal(url);
+    });
 }
 
 app.on("ready", () => {
     createLoadingScreen();
     createHivenClient();
-    app.on("activate", function() {
+    app.on("activate", function () {
         if (BrowserWindow.getAllWindows().length === 0) createHivenClient();
     });
 
 });
 
 app.on("window-all-closed", () => {
-    if(process.platform !== "darwin") {
+    if (process.platform !== "darwin") {
         app.quit();
     }
 });
-
-
-autoUpdater.on('update-downloaded', (event, releaseNotes, releaseName) => {
-    const dialogOpts = {
-        type: 'info',
-        buttons: ['Install', 'Later'],
-        title: 'Application Update',
-        message: process.platform === 'win32' ? releaseNotes : releaseName,
-        detail: 'There is a new version for the Hiven Desktop App. Would you like to install the update?'
-    }
-
-    dialog.showMessageBox(dialogOpts).then((returnValue) => {
-        if (returnValue.response === 0) autoUpdater.quitAndInstall()
-    })
-})
