cd40c35109f05e164d9f9a6c006923431cb63256
devlooskie <hello@looskie.com>
2021-02-04 13:57:57 -0500

    remove this

--- src/main.js
+++ src/main.js
@@ -132,8 +132,7 @@ require("electron").ipcMain.on("nativeLinkCommand", (_, name) => {
 // First, create the loading screen and then the hiven client.
 app.on("ready", () => {
     createLoadingScreen();
-    /* autoUpdater.checkForUpdates(); */
-/*     createHivenClient() */
+    autoUpdater.checkForUpdates();
     if (process.platform === "win32") app.setAppUserModelId("Hiven Canary");
     app.on("activate", function () {
         if (BrowserWindow.getAllWindows().length === 0) createHivenClient();
