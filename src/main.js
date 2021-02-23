ef512181db867f8fb69ae3a368ea9deafdc295b2
devlooskie <hello@looskie.com>
2021-02-23 12:07:33 -0500

    icons in screensharing! and cleaning

--- src/main.js
+++ src/main.js
@@ -2,160 +2,176 @@ const { BrowserWindow, app, dialog } = require("electron");
 const path = require("path");
 const { autoUpdater } = require("electron-updater");
 
-const winStateKeeper = require("./scripts/windowStateKeeper")
+const winStateKeeper = require("./scripts/windowStateKeeper");
 
 // We need this to check if user is trying to open another instance ~DEVLOOSKIE
 const instanceLock = app.requestSingleInstanceLock();
 
 // Disables errors dialogs on production. Check console to Debug.
 dialog.showErrorBox = function (title, content) {
-    console.log(`${title}\n${content}`);
+  console.log(`${title}\n${content}`);
 };
 
 let loadingScreen, hivenClient; // This is here for diasbling the window later on.
 
 function createLoadingScreen() {
-    loadingScreen = new BrowserWindow({
-        width: 300,
-        height: 400,
-        frame: false,
-        transparent: true,
-        resizable: false,
-        webPreferences: {
-            devTools: false
-        }
-    })
-    loadingScreen.loadFile(path.join(__dirname, '/views/loading.html'))
-    loadingScreen.setVisibleOnAllWorkspaces(true);
-    loadingScreen.on('closed', () => {
-        loadingScreen = null;
-    })
-    loadingScreen.webContents.on('did-finish-load', () => {
-        loadingScreen.show();
-    })
+  loadingScreen = new BrowserWindow({
+    width: 300,
+    height: 400,
+    frame: false,
+    transparent: true,
+    resizable: false,
+    webPreferences: {
+      devTools: false,
+    },
+  });
+  loadingScreen.loadFile(path.join(__dirname, "/views/loading.html"));
+  loadingScreen.setVisibleOnAllWorkspaces(true);
+  loadingScreen.on("closed", () => {
+    loadingScreen = null;
+  });
+  loadingScreen.webContents.on("did-finish-load", () => {
+    loadingScreen.show();
+  });
 }
-
 
 function createHivenClient() {
-    let mainWinStateKeeper = new winStateKeeper("main"); // Loads windows size and place from the electron-settings.
-    let winState = mainWinStateKeeper.bounds() // Gets the bound data
-    hivenClient = new BrowserWindow({
-        width: winState.width,
-        height: winState.height,
-        minHeight: 600,
-        minWidth: 980,
-        center: true,
-        resizable: true,
-        frame: false,
-        show: false,
-        webPreferences: {
-            devTools: true,
-            enableRemoteModule: true,
-            nodeIntegration: true,
-            preload: path.join(__dirname, '/scripts/pgdmp.js')
-        }
-    });
+  let mainWinStateKeeper = new winStateKeeper("main"); // Loads windows size and place from the electron-settings.
+  let winState = mainWinStateKeeper.bounds(); // Gets the bound data
+  hivenClient = new BrowserWindow({
+    width: winState.width,
+    height: winState.height,
+    minHeight: 600,
+    minWidth: 980,
+    center: true,
+    resizable: true,
+    frame: false,
+    show: false,
+    webPreferences: {
+      devTools: true,
+      enableRemoteModule: true,
+      nodeIntegration: true,
+      preload: path.join(__dirname, "/scripts/pgdmp.js"),
+    },
+  });
 
-    // ScreenShare Feature
-    hivenClient.webContents.session.setPermissionCheckHandler(async (webContents, permission, details) => {
-        return true
-    })
-    hivenClient.webContents.session.setPermissionRequestHandler(async (webContents, permission, callback, details) => {
-        callback(true)
-    })
+  // ScreenShare Feature
+  hivenClient.webContents.session.setPermissionCheckHandler(
+    async (webContents, permission, details) => {
+      return true;
+    }
+  );
+  hivenClient.webContents.session.setPermissionRequestHandler(
+    async (webContents, permission, callback, details) => {
+      callback(true);
+    }
+  );
 
-    // Loading Hiven
-    hivenClient.loadURL("https://canary.hiven.io");
+  // Loading Hiven
+  hivenClient.loadURL("https://canary.hiven.io");
 
-    mainWinStateKeeper.track(hivenClient); // Track window size to save it.
+  mainWinStateKeeper.track(hivenClient); // Track window size to save it.
 
-    // LoadingScreen Check and Disable
-    hivenClient.webContents.on('did-finish-load', () => {
-        loadingScreen.close();
-        winState.isMaximized ? hivenClient.maximize() : null;
-        hivenClient.show();
-    })
+  // LoadingScreen Check and Disable
+  hivenClient.webContents.on("did-finish-load", () => {
+    loadingScreen.close();
+    winState.isMaximized ? hivenClient.maximize() : null;
+    hivenClient.show();
+  });
 
-
-    // Invite Link Check
-    hivenClient.webContents.on('new-window', async function (e, url) {
-        e.preventDefault();
-        if (url.includes('hiven.house/') || url.includes('hiven.io/invites/')) {
-            let key = await hivenClient.webContents.executeJavaScript("localStorage.getItem('hiven-auth')", true);
-            let link = `https://api.hiven.io/v1/invites/${url.split("/").pop()}`;
-            let request = require('request');
-            await request.get(link, (a, b, c) => {
-                let house_id = JSON.parse(c).data.house.id
-                request.post({
-                    url: link,
-                    headers: {
-                        "Authorization": key
-                    },
-                    method: 'POST'
-                }, (e, r, body) => {
-                    return hivenClient.loadURL(`https://canary.hiven.io/houses/${house_id}`)
-                })
-            })
-            return;
-        }
-        require('electron').shell.openExternal(url);
-    });
+  // Invite Link Check
+  hivenClient.webContents.on("new-window", async function (e, url) {
+    e.preventDefault();
+    if (url.includes("hiven.house/") || url.includes("hiven.io/invites/")) {
+      let key = await hivenClient.webContents.executeJavaScript(
+        "localStorage.getItem('hiven-auth')",
+        true
+      );
+      let link = `https://api.hiven.io/v1/invites/${url.split("/").pop()}`;
+      let request = require("request");
+      await request.get(link, (a, b, c) => {
+        let house_id = JSON.parse(c).data.house.id;
+        request.post(
+          {
+            url: link,
+            headers: {
+              Authorization: key,
+            },
+            method: "POST",
+          },
+          (e, r, body) => {
+            return hivenClient.loadURL(
+              `https://canary.hiven.io/houses/${house_id}`
+            );
+          }
+        );
+      });
+      return;
+    }
+    require("electron").shell.openExternal(url);
+  });
 }
 
-autoUpdater.on('update-not-available', (info) => {
-    loadingScreen.webContents.executeJavaScript(`updateText('Loading Client...')`)
-    createHivenClient()
-})
+autoUpdater.on("update-not-available", (info) => {
+  loadingScreen.webContents.executeJavaScript(
+    `updateText('Loading Client...')`
+  );
+  createHivenClient();
+});
 
-autoUpdater.on('download-progress', (progress) => {
-    loadingScreen.webContents.executeJavaScript(Math.floor(progress.percent))
-})
+autoUpdater.on("download-progress", (progress) => {
+  loadingScreen.webContents.executeJavaScript(Math.floor(progress.percent));
+});
 
-autoUpdater.on('update-downloaded', (info) => {
-    loadingScreen.webContents.executeJavaScript(`updateText('Installing version ${info.version}')`);
-    autoUpdater.quitAndInstall();
-})
+autoUpdater.on("update-downloaded", (info) => {
+  loadingScreen.webContents.executeJavaScript(
+    `updateText('Installing version ${info.version}')`
+  );
+  autoUpdater.quitAndInstall();
+});
 
 require("electron").ipcMain.on("nativeLinkCommand", (_, name) => {
-    switch (name) {
-        case "close":
-            hivenClient.close();
-            break;
+  switch (name) {
+    case "close":
+      hivenClient.close();
+      break;
 
-        case "minimize":
-            hivenClient.minimize();
-            break;
+    case "minimize":
+      hivenClient.minimize();
+      break;
 
-        case "maximize":
-            hivenClient.isMaximized() ? hivenClient.unmaximize() : hivenClient.maximize();
-            break;
-    }
+    case "maximize":
+      hivenClient.isMaximized()
+        ? hivenClient.unmaximize()
+        : hivenClient.maximize();
+      break;
+  }
 });
 
 // Check if user already has an instance open.
 if (!instanceLock) {
-    app.quit();
+  app.quit();
 } else {
-    app.on('second-instance', (e, cL, wD) => {
-        if (hivenClient) {
-            hivenClient.show();
-            hivenClient.focus();
-        }
-    })
+  app.on("second-instance", (_e, _cL, _wD) => {
+    if (hivenClient) {
+      hivenClient.show();
+      hivenClient.focus();
+    }
+  });
 }
 
 // First, create the loading screen and then the hiven client.
 app.on("ready", () => {
-    createLoadingScreen();
-    autoUpdater.checkForUpdates();
-    if (process.platform === "win32") app.setAppUserModelId("Hiven Canary");
-    app.on("activate", function () {
-        if (BrowserWindow.getAllWindows().length === 0) createHivenClient();
-    });
+  createLoadingScreen();
+  autoUpdater.checkForUpdates();
+  if (process.platform === "win32") app.setAppUserModelId("Hiven Canary");
+  app.on("activate", function () {
+    if (BrowserWindow.getAllWindows().length === 0) createHivenClient();
+  });
 });
 
 app.on("window-all-closed", () => {
-    if (process.platform !== "darwin") {
-        app.quit();
-    }
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
--- src/scripts/pgdmp.js
+++ src/scripts/pgdmp.js
@@ -1,14 +1,15 @@
-const { ipcRenderer } = require("electron");
-const localforage = require('localforage');
+const { ipcRenderer, app } = require("electron");
+const localforage = require("localforage");
 window["ipc"] = ipcRenderer;
 
-const { desktopCapturer } = require('electron')
+const { desktopCapturer } = require("electron");
 window.navigator.mediaDevices.getDisplayMedia = () => {
   return new Promise(async (resolve, reject) => {
     try {
-      const uiShitJson = JSON.parse(await localforage.getItem('persist:ui'))
+      const uiShitJson = JSON.parse(await localforage.getItem("persist:ui"));
       const actualUiShit = JSON.parse(uiShitJson.custom_theme);
-      // We need this here
+
+      // We need this here so it doesn't say permission denied
       const style = `<style>
         .remove { animation: fadeOut 150ms }
         .desktop-capturer-selection {
@@ -75,6 +76,16 @@ window.navigator.mediaDevices.getDisplayMedia = () => {
           display: flex;
           margin: 4px;
         }
+        .desktop-capturer-selection__appIcon {
+          width: 25px;
+          height: 25px;
+          background: ${actualUiShit.layoutLighter};
+          padding: 3px;
+          border-radius: 50%;
+          margin-top: -10px;
+          margin-right: -21px;
+          z-index: 1;
+        }
         .desktop-capturer-selection__btn {
           display: flex;
           cursor: pointer;
@@ -124,67 +135,90 @@ window.navigator.mediaDevices.getDisplayMedia = () => {
           }
         }
 
-        </style>`
-      const sources = await desktopCapturer.getSources({ types: ['screen', 'window'] })
+        </style>`;
 
-      const selectionElem = document.createElement('div')
-      selectionElem.classList = 'desktop-capturer-selection'
+      // Get the windows/screens
+      const sources = await desktopCapturer.getSources({
+        types: ["screen", "window"],
+        fetchWindowIcons: true,
+      });
+      console.log(sources);
+      console.log(sources[0].appIcon);
+      // Create element
+      const selectionElem = document.createElement("div");
+
+      // Add class name to it
+      selectionElem.classList = "desktop-capturer-selection";
+
+      // Add style sheet and html skeleton
       selectionElem.innerHTML = `${style}
         <div class="desktop-capturer-selection__scroller">
           <div class="desktop-capturer-selection__container">
           <ul class="desktop-capturer-selection__list">
           <h1 class="desktop-capturer-selection__title">Screen share</h1>
-              ${sources.map(({ id, name, thumbnail, display_id, appIcon }) => `
+              ${sources
+                .map(
+                  ({ id, name, thumbnail, display_id, appIcon }) => `
                 <li class="desktop-capturer-selection__item">
+                  <img class="desktop-capturer-selection__appIcon" src="${
+                    // This ternary expression is realllyyy ugly, but it basically says, if its a screen, put a screen svg, if app doesnt have an icon, put a screen svg as the icon, if everything else passes, put the apps icon as the icon
+                    typeof appIcon != "undefined" && appIcon
+                      ? appIcon.toDataURL() !== "data:image/png;base64,"
+                        ? appIcon.toDataURL()
+                        : `data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='none' stroke='white' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' class='feather feather-monitor'><rect x='2' y='3' width='20' height='14' rx='2' ry='2'></rect><line x1='8' y1='21' x2='16' y2='21'></line><line x1='12' y1='17' x2='12' y2='21'></line></svg>`
+                      : `data:image/svg+xml;utf8,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24' fill='none' stroke='white' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' class='feather feather-monitor'><rect x='2' y='3' width='20' height='14' rx='2' ry='2'></rect><line x1='8' y1='21' x2='16' y2='21'></line><line x1='12' y1='17' x2='12' y2='21'></line></svg>`
+                  }" />
                   <button class="desktop-capturer-selection__btn" data-id="${id}" title="${name}">
                     <img class="desktop-capturer-selection__thumbnail" src="${thumbnail.toDataURL()}" />
                     <span class="desktop-capturer-selection__name">${name}</span>
                   </button>
                 </li>
-              `).join('')}
+              `
+                )
+                .join("")}
               <button onclick="const selectionElem = document.querySelector('.desktop-capturer-selection'); selectionElem.classList.add('remove'); setTimeout(() => selectionElem.remove(), 140)" class="desktop-capturer-selection__close">Cancel</button>
             </ul>
           </div>
         </div>
-      `
-      document.body.appendChild(selectionElem)
-      document.addEventListener('keydown', closeWindow);
-      document.querySelectorAll('.desktop-capturer-selection__btn')
-        .forEach(button => {
-          button.addEventListener('click', async () => {
+      `;
+      document.body.appendChild(selectionElem);
+      document.addEventListener("keydown", closeWindow, { once: true });
+      document
+        .querySelectorAll(".desktop-capturer-selection__btn")
+        .forEach((button) => {
+          button.addEventListener("click", async () => {
             try {
-              const id = button.getAttribute('data-id')
-              const source = sources.find(source => source.id === id)
+              const id = button.getAttribute("data-id");
+              const source = sources.find((source) => source.id === id);
               if (!source) {
-                throw new Error(`Source with id ${id} does not exist`)
+                throw new Error(`Source with id ${id} does not exist`);
               }
 
               const stream = await window.navigator.mediaDevices.getUserMedia({
                 audio: false,
                 video: {
                   mandatory: {
-                    chromeMediaSource: 'desktop',
-                    chromeMediaSourceId: source.id
-                  }
-                }
-              })
-              resolve(stream)
+                    chromeMediaSource: "desktop",
+                    chromeMediaSourceId: source.id,
+                  },
+                },
+              });
+              resolve(stream);
 
               closeWindow();
             } catch (err) {
-              console.error('Error selecting desktop capture source:', err)
-              reject(err)
+              console.error("Error selecting desktop capture source:", err);
+              reject(err);
             }
-          })
-        })
-      function closeWindow(e) {
-        this.removeEventListener('keydown', closeWindow); // Remove event listener 
-        selectionElem.classList.add('remove');
-        setTimeout(() => selectionElem.remove(), 140) // Wait to get rid of the element before the animation stops!
+          });
+        });
+      function closeWindow(_e) {
+        selectionElem.classList.add("remove");
+        setTimeout(() => selectionElem.remove(), 140); // Wait to get rid of the element before the animation stops!
       }
     } catch (err) {
-      console.error('Error displaying desktop capture sources:', err)
-      reject(err)
+      console.error("Error displaying desktop capture sources:", err);
+      reject(err);
     }
-  })
-}
+  });
+};
